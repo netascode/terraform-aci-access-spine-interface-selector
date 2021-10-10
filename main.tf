259748393b6ebce6688b73963713cdd33724dd5e
danischm <danischm@cisco.com>
2021-10-10 17:11:52 +0200

    Update implicit resource dependencies

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aci_rest" "infraSHPortS" {
 
 resource "aci_rest" "infraRsSpAccGrp" {
   count      = var.policy_group != null ? 1 : 0
-  dn         = "${aci_rest.infraSHPortS.id}/rsspAccGrp"
+  dn         = "${aci_rest.infraSHPortS.dn}/rsspAccGrp"
   class_name = "infraRsSpAccGrp"
   content = {
     tDn = "uni/infra/funcprof/spaccportgrp-${var.policy_group}"
@@ -18,7 +18,7 @@ resource "aci_rest" "infraRsSpAccGrp" {
 
 resource "aci_rest" "infraPortBlk" {
   for_each   = { for block in var.port_blocks : block.name => block }
-  dn         = "${aci_rest.infraSHPortS.id}/portblk-${each.value.name}"
+  dn         = "${aci_rest.infraSHPortS.dn}/portblk-${each.value.name}"
   class_name = "infraPortBlk"
   content = {
     name     = each.value.name
